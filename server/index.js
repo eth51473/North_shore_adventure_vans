68feebd8f2f356688c865b09369adcad6a8c689c
Ethan Bird <eth51473@gmail.com>
2021-09-12 19:03:22 -0600

    added get for the img folder in server file

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ app.get('/', (req,res) =>{
 app.get('/css',(req,res) => {
   res.sendFile(path.join(__dirname, '../styles.css'))
 })
+app.get('/img',(req,res) => {
+  res.sendFile(path.join(__dirname, '../img'))
+})
+
 // app.use(express.static("web-hosting"))
 
 
