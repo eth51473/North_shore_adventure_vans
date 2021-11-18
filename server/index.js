04283bc1b245befe2f291ac20cf89b4f9f652eae
Ethan Bird <eth51473@gmail.com>
2021-11-18 10:54:31 -0700

    updated meta image tag for links

--- server/index.js
+++ server/index.js
@@ -11,15 +11,6 @@ app.get('/css',(req,res) => {
   res.sendFile(path.join(__dirname, '../styles.css'))
 })
 
-// app.get('/img',(req,res) => {
-//   res.sendFile(path.join(__dirname, '../img'))
-// })
-// app.use(express.static('img'))
-// app.use(express.static(path.join(__dirname, 'img')))
-
-// app.use(express.static("web-hosting"))
-
-
 const port = process.env.PORT || 4005
 
 app.listen(port, ()=>{
